bd0d9991b59ecea60b0e19596547ba4ae0981092
alvaritomass <alvaro.s366333@cesurformacion.com>
2024-10-28 14:22:57 +0100

    28.10(script ataque de espada y pruebas)

--- IslasTascas/Assets/Scripts/Alvaro/MovimientoTomi.cs
+++ IslasTascas/Assets/Scripts/Alvaro/MovimientoTomi.cs
@@ -9,6 +9,9 @@ public class MovimientoTomi : MonoBehaviour
     float moveY;
     float moveX;
     public Rigidbody2D rb;
+
+    Animator animator;
+    bool IsAttacking;
     void Start()
     {
         
@@ -17,10 +20,24 @@ public class MovimientoTomi : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         moveX = Input.GetAxis("Horizontal");
-         moveY = Input.GetAxis("Vertical");
-         rb.velocity = new Vector2(moveX, moveY).normalized * velocidad * Time.deltaTime;
+        Movement();
     }
 
+    private void Movement()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(moveX, moveY).normalized * velocidad * Time.deltaTime;
 
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            animator.Play("Ataque");
+            IsAttacking = true; 
+        }
+    }
+
+    private void EndAttack()
+    {
+        IsAttacking = false;
+    }
 }
